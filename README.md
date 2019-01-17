dc0d6d8b64cc0603cb5c64924ec29550a7989f9a
Xavier Heroult <xavier.heroult@gmail.com>
2019-01-17 17:36:29 +0100

    conf for attiny85

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # arduino-washing-machine-delayed-start
+
+## Documentation
+* https://create.arduino.cc/projecthub/arjun/programming-attiny85-with-arduino-uno-afb829
--- washing-machine/washing-machine.ino
+++ washing-machine/washing-machine.ino
@@ -1,16 +1,16 @@
 #include <Button.h>
 
-#define STATUS_LED 13
-#define ON_OFF 2
-#define PLAY_PAUSE 3
+#define BUTTON_SETUP 0
+#define BUTTON_PLAY 1
+#define STATUS_LED 2
 
-#define BUTTON_SETUP 4
-#define BUTTON_PLAY 5
+#define ON_OFF 3
+#define PLAY_PAUSE 4
 
 #define TIME_DISPLAY 200
 #define MAX_HOURS 9
 #define DEFAULT_HOURS 1
-#define SECONDS_PER_HOUR 30
+#define SECONDS_PER_HOUR 10
 
 #define ON_OFF_TIME 1000
 #define PAUSE_BETWEEN_ACTIONS 4000
@@ -18,30 +18,7 @@
 
 #define MAIN_LOOP_SECONDS 2000
 
-
 int requested_hours;
-
-Button setup_button = Button(BUTTON_SETUP, BUTTON_PULLDOWN);
-Button play_button = Button(BUTTON_PLAY, BUTTON_PULLDOWN);
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(STATUS_LED, OUTPUT);
-  pinMode(ON_OFF, OUTPUT);
-  pinMode(PLAY_PAUSE, OUTPUT);
-  
-  Serial.begin(9600);
-  requested_hours = DEFAULT_HOURS;
-  
-  while (!play_button.isPressed()) {
-    if (setup_button.isPressed()) {
-      requested_hours++;
-      if (requested_hours > MAX_HOURS) {
-        requested_hours = 1;
-      }
-      blink(requested_hours, STATUS_LED);
-    }
-  }
-}
 
 void blink(int time, int dest) {
   for (int i = 0; i < time; i++) {
@@ -68,6 +45,28 @@ void endLoop() {
   }
 }
 
+void setup() {
+  Button setup_button = Button(BUTTON_SETUP, BUTTON_PULLDOWN);
+  Button play_button = Button(BUTTON_PLAY, BUTTON_PULLDOWN);
+  
+  pinMode(STATUS_LED, OUTPUT);
+  pinMode(ON_OFF, OUTPUT);
+  pinMode(PLAY_PAUSE, OUTPUT);
+  
+  Serial.begin(9600);
+  requested_hours = DEFAULT_HOURS;
+  
+  while (!play_button.isPressed()) {
+    if (setup_button.isPressed()) {
+      requested_hours++;
+      if (requested_hours > MAX_HOURS) {
+        requested_hours = 1;
+      }
+      blink(requested_hours, STATUS_LED);
+    }
+  }
+}
+
 void loop() {
   int remaining_seconds = 0;
   int remaining_hours = 0;
@@ -76,6 +75,7 @@ void loop() {
   while (true) {
     remaining_seconds = endTime - (millis() / 1000);
     remaining_hours = remaining_seconds / SECONDS_PER_HOUR;
+    
     Serial.print(endTime);
     Serial.print(" - ");
     Serial.println(remaining_seconds);
