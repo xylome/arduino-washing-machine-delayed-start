9de55783b33b8035bf9a58b79a5984400276fbbd
Xavier Heroult <xavier.heroult@gmail.com>
2019-01-18 11:58:55 +0100

    First usable version

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-# arduino-washing-machine-delayed-start
+# Arduino washing machine delayed start
+This is a little circuit to delay the start of a washing machine.
+Can be used to delay anything via an optocoupler.
+
+## How to use
+When powering on the circuit, the status LED shows the default number of hours it will wait before trigering the optocoupler. Left button allows to increment that number by one hour on each pression (max 9 hours). Once set, the countdown starts when you press the button on the right.
+
+The status LED shows the remaining hours left until the triger.
+
+The start of my washing machine consists in two steps :
+* swith on;
+* start washing;
+
+Hence, the 2 channel optocoupler. A status LED is linked with each channel to allow troubleshooting.
+
+When everything is done, the status LED will blink several times each 5 seconds to let you know you have to switch off the circuit.
+
+## Parts list
+* arduino :
+  - development platform : arduino nano
+  - target platform : attiny85
+* optocoupler : ILS74 https://www.vishay.com/docs/83640/ild74.pdf
+* 3 LEDS
+* resistors
+  - 3 * 330 ohms for the LEDS
+  - 2 * 10k ohms for pullups
+* 1 * lipo battery (3.7 V)
+
+## Platform configuration
+### development
+  * type of card : Arduino nano
+  * processor : Atmega 328P (old bootloader)
+  * programmer : ArduinoISP
+
+### target
+  * type of card : ATtiny25/45/85
+  * processor : attiny85
+  * clock : internal 1MHz
+  * programmer : Arduino as ISP
 
 ## Documentation
-* https://create.arduino.cc/projecthub/arjun/programming-attiny85-with-arduino-uno-afb829
+To use arduino as ISP, I followed instructions available here : https://create.arduino.cc/projecthub/arjun/programming-attiny85-with-arduino-uno-afb829
--- washing-machine/washing-machine.ino
+++ washing-machine/washing-machine.ino
@@ -1,22 +1,50 @@
+/*
+Copyright (C) 2019  Xavier Héroult <xavier@placard.fr.eu.org>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 #include <Button.h>
 
+#if defined (__AVR_ATtiny85__)
 #define BUTTON_SETUP 0
 #define BUTTON_PLAY 1
 #define STATUS_LED 2
-
 #define ON_OFF 3
 #define PLAY_PAUSE 4
+#else
+#define BUTTON_SETUP 4 
+#define BUTTON_PLAY 5
+#define STATUS_LED LED_BUILTIN
+#define ON_OFF 2
+#define PLAY_PAUSE 3
+#define DEBUG ON
+#endif
 
 #define TIME_DISPLAY 200
 #define MAX_HOURS 9
-#define DEFAULT_HOURS 1
-#define SECONDS_PER_HOUR 10
+#define DEFAULT_HOURS 4
+#define SECONDS_PER_HOUR 60
 
 #define ON_OFF_TIME 1000
 #define PAUSE_BETWEEN_ACTIONS 4000
 #define PLAY_PAUSE_TIME 1000
 
 #define MAIN_LOOP_SECONDS 2000
+#define END_BLINK_TIME 15
+#define END_LOOP_PAUSE 5000
 
 int requested_hours;
 
@@ -29,7 +57,7 @@ void blink(int time, int dest) {
   }
 }
 
-void startWashingMachine() {
+void start_washing_machine() {
   digitalWrite(ON_OFF, HIGH);
   delay(ON_OFF_TIME);
   digitalWrite(ON_OFF, LOW);
@@ -39,9 +67,19 @@ void startWashingMachine() {
   digitalWrite(PLAY_PAUSE, LOW);
 }
 
-void endLoop() {
+void end_display(int dest) {
+  for (int i = 0; i < END_BLINK_TIME; i++) {
+    digitalWrite(dest, HIGH);
+    delay(100);
+    digitalWrite(dest, LOW);
+    delay(100);
+  }
+}
+
+void end_loop() {
   while(true) {
-    delay(10000);
+    end_display(STATUS_LED);
+    delay(END_LOOP_PAUSE);
   }
 }
 
@@ -53,8 +91,13 @@ void setup() {
   pinMode(ON_OFF, OUTPUT);
   pinMode(PLAY_PAUSE, OUTPUT);
   
+#if defined(DEBUG)
   Serial.begin(9600);
+#endif
   requested_hours = DEFAULT_HOURS;
+  
+  // show us the default setting
+  blink(requested_hours, STATUS_LED);
   
   while (!play_button.isPressed()) {
     if (setup_button.isPressed()) {
@@ -70,19 +113,33 @@ void setup() {
 void loop() {
   int remaining_seconds = 0;
   int remaining_hours = 0;
-  int endTime = requested_hours * SECONDS_PER_HOUR;
+  int end_time = (millis() / 1000) + requested_hours * SECONDS_PER_HOUR - 1;
+#if defined(DEBUG)
+  Serial.println("Main loop");
+  Serial.print("requestes hours: ");
+  Serial.print(requested_hours);
+  Serial.print("h - endTime: ");
+  Serial.print(end_time);
+  Serial.println("s");
+#endif
   
   while (true) {
-    remaining_seconds = endTime - (millis() / 1000);
+    remaining_seconds = end_time - (millis() / 1000);
     remaining_hours = remaining_seconds / SECONDS_PER_HOUR;
-    
-    Serial.print(endTime);
-    Serial.print(" - ");
-    Serial.println(remaining_seconds);
-
+#if defined(DEBUG)
+    Serial.print("remaining secsonds: ");
+    Serial.print(remaining_seconds);
+    Serial.print("s - remaining hours: ");
+    Serial.print(remaining_hours);
+    Serial.print("h - end time: ");
+    Serial.print(end_time);
+    Serial.print("s - now: ");
+    Serial.print(millis()  / 1000);
+    Serial.println("s");
+#endif
     if (remaining_seconds <= 0) {
-      startWashingMachine();
-      endLoop();
+      start_washing_machine();
+      end_loop();
     } else {
       blink(remaining_hours + 1, STATUS_LED);
     }
